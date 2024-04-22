76235ecbaac4ec0e0445616a7e8a752b4f5a3537
Dominic Willi <dominic.willi@students.bfh.ch>
2024-04-22 12:46:35 +0200

    json

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: "/plugin/spa",
+      publicPath: "/plugin/revival/spa",
       // analyze: true,
       // env: {},
       // rawDefine: {}
